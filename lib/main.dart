c5ef5677891a4e69ee4804212d0b3f6594a89c6c
MinXiang <zack850813@gmail.com>
2020-10-10 23:29:17 +0800

    Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Deme Flutter'),
+      home: MyHomePage(title: 'Hello World'),
     );
   }
 }
